39035988b877d5bce201e10d1b0a73648ea0b85d
Michel Valenzuela <michel.valenzuela6@gmail.com>
2023-11-24 23:20:47 -0300

    devise

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get pages_home_url
     assert_response :success
   end
+
+  test "should get library" do
+    get pages_library_url
+    assert_response :success
+  end
 end
